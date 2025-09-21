324eabb503c7257275a1c870439574605844ebc8
Pedro <pcrigotto@gmail.com>
2025-09-20 22:52:44 -0300

    load online

--- Assets/SaveManager.cs
+++ Assets/SaveManager.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.InputSystem;
+using UnityEngine.Networking;
 
 public class SaveManager : MonoBehaviour
 {
@@ -20,13 +21,18 @@ public class SaveManager : MonoBehaviour
 
     void Update()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Keyboard.current.lKey.wasPressedThisFrame)
         {
             Load();
-        }
-        else if (Keyboard.current.sKey.wasPressedThisFrame)
+        } else if (Keyboard.current.sKey.wasPressedThisFrame)
         {
             Save();
+        } else if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            StartCoroutine("LoadOnline");
+        } else if (Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            Reset();
         }
     }
 
@@ -69,5 +75,38 @@ public class SaveManager : MonoBehaviour
         cube2.transform.localScale = sceneData.cubes[1].scale;
         sphere1.transform.position = sceneData.spheres[0].position;
         sphere2.transform.position = sceneData.spheres[1].position;
+    }
+
+    void Reset()
+    {
+        cube1.transform.position = new Vector3(0, 0, 0);
+        cube1.transform.localScale = new Vector3(0, 0, 0);
+        cube2.transform.position = new Vector3(0, 0, 0);
+        cube2.transform.localScale = new Vector3(0, 0, 0);
+        sphere1.transform.position = new Vector3(0.095f, 0, 0.155f);
+        sphere2.transform.position = new Vector3(0, 0, 0.148f);
+    }
+
+    System.Collections.IEnumerator LoadOnline()
+    {
+        string url = "https://raw.githubusercontent.com/Pedro-Rigotto/trabalho2backend/refs/heads/main/save.json";
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            string json = request.downloadHandler.text;
+            Adapter.SceneData sceneData = JsonUtility.FromJson<Adapter.SceneData>(json);
+            cube1.transform.position = sceneData.cubes[0].position;
+            cube1.transform.localScale = sceneData.cubes[0].scale;
+            cube2.transform.position = sceneData.cubes[1].position;
+            cube2.transform.localScale = sceneData.cubes[1].scale;
+            sphere1.transform.position = sceneData.spheres[0].position;
+            sphere2.transform.position = sceneData.spheres[1].position;
+            Debug.Log("Online data loaded successfully from url:\n" + url);
+        }
+        else
+        {
+            Debug.LogError("Failed to load online data: " + request.error);
+        }
     }
 }
